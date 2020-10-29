ec7389329ba8c8f197ca18738ad99c7dba1984a5
Jake Crawford <jjc2718@gmail.com>
2020-10-29 11:39:41 -0400

    add cancer type count to sample sizes script

--- pancancer_evaluation/scripts/get_sample_sizes.py
+++ pancancer_evaluation/scripts/get_sample_sizes.py
@@ -10,11 +10,20 @@ import pancancer_evaluation.config as cfg
 from pancancer_evaluation.data_models.tcga_data_model import TCGADataModel
 import pancancer_evaluation.utilities.data_utilities as du
 
-def get_num_samples_for_gene(identifier, classification, tcga_data):
-    # process data for given gene
+def count_samples_for_gene(identifier, classification, tcga_data):
     tcga_data.process_train_data_for_gene(gene, classification, None)
     assert tcga_data.X_train_raw_df.shape[0] == tcga_data.y_train_df.shape[0]
-    return tcga_data.y_train_df.shape[0]
+    id_counts_df = (
+        tcga_data.y_train_df.groupby('DISEASE')
+                            .count()
+                            .reset_index()
+                            .rename({'status': 'count', 'DISEASE': 'cancer_type'},
+                                    axis='columns')
+                            .assign(identifier=lambda x: gene + '_' + x.cancer_type)
+    )
+    id_counts_df.drop(id_counts_df.columns.difference(['identifier', 'count']),
+                      axis='columns', inplace=True)
+    return id_counts_df
 
 
 if __name__ == '__main__':
@@ -25,19 +34,20 @@ if __name__ == '__main__':
     genes_df = du.load_vogelstein()
     all_genes = genes_df.gene.values
 
-    sample_sizes = []
+    id_counts_df = pd.DataFrame()
 
     for gene in tqdm.tqdm(all_genes):
 
         try:
             classification = du.get_classification(gene, genes_df)
-            # TODO: num samples for each cancer type?
-            num_samples = get_num_samples_for_gene(gene, classification, tcga_data)
-            sample_sizes.append([gene, num_samples])
+            id_counts_df = pd.concat((
+                id_counts_df,
+                count_samples_for_gene(gene, classification, tcga_data)
+            ))
         except KeyError:
             # can happen for genes that aren't in mutation data, if so skip
+            print('Skipping gene {}, not in mutation data'.format(gene))
             continue
 
-    sample_size_df = pd.DataFrame(sample_sizes, columns=['gene', 'num_samples'])
-    sample_size_df.to_csv('test.tsv', sep='\t')
+    id_counts_df.to_csv('test.tsv', sep='\t', index=False)
 
